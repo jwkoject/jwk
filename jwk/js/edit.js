f56753ce6098eeafd45bec3bc8fa5fd7abab4df8
图片 <s476946980@users.noreply.github.com>
2017-11-07 11:09:14 +0800

    上传图片更新

--- jwk/js/edit.js
+++ jwk/js/edit.js
@@ -1,31 +1,16 @@
-/* 
-* @Author: anchen
-* @Date:   2017-11-05 18:34:52
-* @Last Modified by:   anchen
-* @Last Modified time: 2017-11-05 18:46:29
-*/
-
-$(document).ready(function(){
-    var file = document.getElementById("files");
-    var imgfile = document.getElementById("imgfile");
-    function imgChange(obj1, obj2) {
-        //获取点击的文本框
-        //存放图片的父级元素
-        var imgContainer = document.getElementsByClassName(obj1)[0];
-
-        //获取的图片文件
-        var fileList = file.files;
-        //文本框的父级元素
-        var input = document.getElementsByClassName(obj2)[0];
-        var imgArr = [];
-        for (var i = 0; i < fileList.length; i++) {
-            var imgUrl = window.URL.createObjectURL(file.files[i]);
-            imgArr.push(imgUrl);
-            imgfile.setAttribute("src",imgArr[i]);
-            console.log(imgArr[i])
-        };
-    };
-    file.onchange = function() {
-        imgChange('z_photo','z_file');
-    }
-});
+ function previewImage(file){
+    var MAXWIDTH  = 90; 
+    var MAXHEIGHT = 90;
+    var div = document.getElementById('preview');
+    div.innerHTML ='<img id=imghead onclick=$("#previewImg").click()>';
+          var img = document.getElementById('imghead');
+          img.onload = function(){
+            var rect = clacImgZoomParam(MAXWIDTH, MAXHEIGHT, img.offsetWidth, img.offsetHeight);
+            img.width  =  rect.width;
+            img.height =  rect.height;
+            img.style.marginTop = rect.top+'px';
+          }
+          var reader = new FileReader();
+          reader.onload = function(evt){img.src = evt.target.result;}
+          reader.readAsDataURL(file.files[0]);
+}

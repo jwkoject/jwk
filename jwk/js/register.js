540fb1d14c8d1c3fd71ba0e0208c88c61393f279
图片 <s476946980@users.noreply.github.com>
2017-11-07 13:52:55 +0800

    注册与注册信息合并
    注册与注册信息合并

--- jwk/js/register.js
+++ jwk/js/register.js
@@ -4,6 +4,14 @@ $("#button").click(function(){
     }
     if($("#psd").val()!==$("#psdcon").val()|| $("#psd").val()=="" ||$("#psdcon").val()=="") {
         $(".password-remind").slideDown();
+    } else {
+        $(".page-first").hide()
+        $(".page-secend").show()
+        $("h2").show()
     }
-    // window.location.href="register_information.html"
+})
+$(".back").click(function(){
+    $(".page-first").show()
+    $(".page-secend").hide()
+    $("h2").hide()
 })
